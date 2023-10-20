412202a3ad5e553f2972adfd90707e0df6a1bbd8
Maximilian <mposnikmle@gmail.com>
2023-10-20 15:08:16 -0400

    finished endpoints in room controller

--- server/controllers/room.controller.js
+++ server/controllers/room.controller.js
@@ -25,4 +25,56 @@ router.post("/createRoom", validateSession, async (req, res) => {
     }
 })
 
+router.get("/displayAllRooms", validateSession, async (req, res) => {
+    try {
+        const rooms = await Room.find();
+
+        res.json({message: "diplaying all rooms", rooms: rooms});
+    } catch (error) {
+        res.status(500).json({ message: error.message, });
+    }
+});
+
+router.patch("/updateRoom/:id", validateSession, async (req, res) => {
+    try {
+        const id = req.params.id;
+        const conditions = { _id: id, ownerId: req.user._id };
+        const data = req.body;
+        const options = { new: true };
+
+        const room = await Room.findOneAndUpdate(conditions, data, options);
+
+        if (!room) {
+            throw new Error("Room was not found");
+        }
+
+        res.json({ message: "Room Updated", room: room, })
+
+    } catch (error) {
+        res.status(500).json({ message: error.message, });
+    }
+})
+
+router.delete("/deleteRoom/:id", validateSession, async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const conditions = {
+            _id: id,
+            ownerId: req.user._id,
+        };
+
+        const room = await Room.deleteOne(conditions);
+
+        res.json({
+            message:
+                room.deletedCount === 1
+                ? "Room was successfully deleted"
+                : "FAILURE room not deleted"
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message, });
+    }
+})
+
 module.exports = router;
