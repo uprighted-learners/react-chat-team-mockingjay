40793e6362a062ac327dfd4d58042969c9f46bf6
just-Joey <far.field@gmail.com>
2023-10-20 14:44:28 -0600

    added creat message endpoint

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -1,3 +1,26 @@
 const router = require("express").Router();
-// const Message = require("../models/message.model");
+const Message = require("../models/message.model");
+const validateSession = require("../middleware/validate-session");
+
+router.post("/createMessage", validateSession, async (req, res) => {
+  try {
+    const roomId = req.params.id;
+    const { user, body } = req.body;
+    const newMessage = new Message({
+      user: user,
+      body: body,
+      ownerId: req.user._id,
+      roomId: roomId,
+    });
+
+    await newMessage.save();
+    res.json({
+      message: "message created successfully",
+      newMessage: newMessage,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 module.exports = router;
