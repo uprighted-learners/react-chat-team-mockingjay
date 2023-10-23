7ae5254589167e6f9eec18642dceb57058776205
Steven Kurczek <leed99489@gmail.com>
2023-10-23 16:13:53 -0400

    steves diverging

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -3,26 +3,27 @@ const Message = require("../models/message.model");
 const validateSession = require("../middleware/validate-session");
 
 router.post("/createMessage", validateSession, async (req, res) => {
-  try {
-    const roomId = req.params.id;
-    const { user, body } = req.body;
-    const newMessage = new Message({
-      user: user,
-      body: body,
-      ownerId: req.user._id,
-      roomId: roomId,
-    });
+    try {
+        const roomId = req.params.id;
+        const { user, body } = req.body;
+        const newMessage = new Message({
+        user: user,
+        body: body,
+        ownerId: req.user._id,
+        roomId: roomId,
+        });
 
-    await newMessage.save();
-    res.json({
-      message: "message created successfully",
-      newMessage: newMessage,
-    });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+        await newMessage.save();
+        res.json({
+        message: "message created successfully",
+        newMessage: newMessage,
+        });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 });
 
+<<<<<<< HEAD
 router.get("/view-all",  async (req, res) => {
     try {
       console.log("req.user", req.user);
@@ -37,6 +38,11 @@ router.get("/view-all",  async (req, res) => {
   });
 
 
+=======
+
+
+module.exports = router;
+>>>>>>> a538fa510a9445a4603f741f02d00e55d3b6dd25
 
 // const Message = require("../models/message.model");
 module.exports = router;
