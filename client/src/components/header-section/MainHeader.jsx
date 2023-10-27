0e8386fdf3eb7019680d20a334e13aea101aa035
Maximilian <mposnikmle@gmail.com>
2023-10-27 15:10:42 -0400

    Created section filepaths and a header/navbar

---
+++ client/src/components/header-section/MainHeader.jsx
@@ -0,0 +1,16 @@
+// TODO: CHANGE BEFORE SUBMITTING
+
+import "./MainHeader.css";
+function MainHeader(props) {
+    return (
+        <>
+            <header className="d-flex justify-content-center align-items-center secondary-background">
+            <div className="d-flex justify-content-center align-items-center primary-background header-stripe w-100">
+            {/* <img src={logo} alt="logo" className="App-logo" /> */}
+            </div>
+        </header>
+        </>
+    );
+}
+
+export default MainHeader;
