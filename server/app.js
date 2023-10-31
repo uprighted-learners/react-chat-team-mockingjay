10954834b443f770f342f1ef93128c2da4da84a8
Steven Kurczek <leed99489@gmail.com>
2023-10-31 11:09:41 -0400

    toggleBUtton

--- server/app.js
+++ server/app.js
@@ -10,12 +10,17 @@ const mongoose = require("mongoose");
 const PORT = process.env.PORT;
 const DBNAME = process.env.DBNAME;
 const DB_URL = process.env.DB_URL;
+const cors = require("cors");
 
 mongoose.connect(DB_URL + DBNAME);
 const db = mongoose.connection;
 db.once("open", () => {
   console.log("connected to the DB", DBNAME);
 });
+
+app.use(cors({
+  origin: '*',
+}));
 
 app.use(express.json());
 
