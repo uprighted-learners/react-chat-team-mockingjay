620daf7b5561b01c4c1e4d9e197893b6662ec182
just-Joey <far.field@gmail.com>
2023-10-30 19:07:56 -0600

    minor css changes to mainheader and auth including header photo and color palette

--- client/src/components/authorization-section/Auth.jsx
+++ client/src/components/authorization-section/Auth.jsx
@@ -1,19 +1,15 @@
 import Login from "./Login";
 import Register from "./CreateUser";
-
+import "./Auth.css";
 function Auth(props) {
   return (
-    <><h1>Hello from Auth</h1>
-    
-    <Login     />
-    <Register   />
-    
-    
-    
+    <>
+      <h1>Hello from District One</h1>
+
+      <Login />
+      <Register />
     </>
-    
   );
 }
-
 
 export default Auth;
