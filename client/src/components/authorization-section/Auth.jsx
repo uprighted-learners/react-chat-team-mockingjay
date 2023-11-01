78a751f86a4533ebbb1e49eacd411035340121cf
just-Joey <far.field@gmail.com>
2023-11-01 11:07:23 -0600

    changed main font and added it to Auth CreateUser and Login components

--- client/src/components/authorization-section/Auth.jsx
+++ client/src/components/authorization-section/Auth.jsx
@@ -3,21 +3,10 @@ import Register from "./CreateUser";
 import "./Auth.css";
 function Auth(props) {
   return (
-<<<<<<< HEAD
     <>
       <h1>Hello from District One</h1>
-
-      <Login />
+      <Login updateToken={props.updateToken} />
       <Register />
-=======
-    <><h1>Hello from Auth</h1>
-    
-    <Login  updateToken={props.updateToken}   />
-    <Register   />
-    
-    
-    
->>>>>>> 83beea809de937cb8fae8fa539618a313f08975c
     </>
   );
 }
--- client/src/components/authorization-section/Login.jsx
+++ client/src/components/authorization-section/Login.jsx
@@ -1,96 +1,86 @@
-
 import React, { useState } from "react";
 import { Form, FormGroup, Label, Input } from "reactstrap";
 import OurButton from "../../ui/OurButton";
 import { API_USER_LOGIN } from "../../constants/endpoints";
-
+import "./Auth.css";
 
 function Login(props) {
-    const [email, setEmail] = useState("jwick@puppyfinder.com")
-    const [password, setPassword] = useState("focusCommitment1979")
+  const [email, setEmail] = useState("jwick@puppyfinder.com");
+  const [password, setPassword] = useState("focusCommitment1979");
 
+  async function handleSubmit() {
+    try {
+      // HEADERS
+      let myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      //BODY
 
-    async function handleSubmit() {
-try {
-        // HEADERS
-        let myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json")
-        //BODY
-        
-    const body = 
-    {
+      const body = {
         email: email,
-        password: password
-    }
-        // REQUEST OPTIONS
-    // console.log(body)
-        const requestOption ={
-                method: "POST",
-                headers: myHeaders,
-                body: JSON.stringify(body)
-        };
-        console.log(requestOption)
-        //SEND REQUEST
-        const response = await fetch(API_USER_LOGIN, requestOption)
-        // GET A RESPONSE
-        const data = await response.json()
+        password: password,
+      };
+      // REQUEST OPTIONS
+      // console.log(body)
+      const requestOption = {
+        method: "POST",
+        headers: myHeaders,
+        body: JSON.stringify(body),
+      };
+      console.log(requestOption);
+      //SEND REQUEST
+      const response = await fetch(API_USER_LOGIN, requestOption);
+      // GET A RESPONSE
+      const data = await response.json();
 
-        //UPDATE THE TOKEN
-        props.updateToken(data.token)
-    
-} catch (error) {
-    console.error(error)
-}
+      //UPDATE THE TOKEN
+      props.updateToken(data.token);
+    } catch (error) {
+      console.error(error);
     }
+  }
 
   return (
     <>
-        <div className="d-flex justify-content-center mt-5">
-        <div 
-        className="secondary-background p-5 rounded"
-        style={{width:"450px",height: "370ppx"}}
->
-
-         <h2 className="text-center font-primary bold"> Login Form</h2>
-         <Form>
+      <div className="d-flex justify-content-center mt-5">
+        <div
+          className="secondary-background p-5 rounded"
+          style={{ width: "450px", height: "370ppx" }}
+        >
+          <h2>Login Form</h2>
+          <Form>
             {/* Form group Email */}
             <FormGroup>
-            <Label for="email"> Email  </Label>
-             <Input
-                 id="email"
-                 name="email"
-                 placeholder="Please Enter Email"
-                 type="email"    
-                 value={email}
-                 onChange={(e) => setEmail(e.target.value)}
-                 />
+              <Label for="email"> Email </Label>
+              <Input
+                id="email"
+                name="email"
+                placeholder="Please Enter Email"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </FormGroup>
-        {/* Form Group email ending */ }
+            {/* Form Group email ending */}
 
             {/* Form group password */}
             <FormGroup>
-            <Label for="password"> Password  </Label>
-             <Input
-                 id="password"
-                 name="password"
-                 placeholder="Enter Password"
-                 type="password" 
-                 value={password}
-                 onChange={(e) => setPassword(e.target.value)}
-                 />
+              <Label for="password"> Password </Label>
+              <Input
+                id="password"
+                name="password"
+                placeholder="Enter Password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </FormGroup>
-             {/* Form group password ending */}
-             <OurButton
-              title="Login"
-              onClick={handleSubmit}
-            />
-
-         </Form>
-</div>
-    </div>   
+            {/* Form group password ending */}
+            <OurButton title="Login" onClick={handleSubmit} />
+          </Form>
+        </div>
+      </div>
     </>
   );
 }
-
 
 export default Login;
